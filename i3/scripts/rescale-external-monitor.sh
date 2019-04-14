01dd4cd2a7b58af8ed793b8c4667805bfe8fb84a
Sven <elflein.sven@googlemail.com>
2019-04-14 14:56:10 +0200

    Multi monitor config, py3status

---
+++ i3/scripts/rescale-external-monitor.sh
@@ -0,0 +1,3 @@
+xrandr --dpi 276 --fb 3840x2160 \
+    --output eDP1 --off \
+    --output DP2 --scale 1.5x1.5 --panning 3840x2160
---
+++ i3/scripts/rescale-external-two-monitors.sh
@@ -0,0 +1,3 @@
+xrandr --dpi 276 --fb 6840x4160 \
+    --output eDP1 --mode 3000x2000 \
+    --output DP2 --scale 1.5x1.5 --pos 3000x0 --panning 3840x2160+3000+0
